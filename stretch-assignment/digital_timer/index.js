0a5c6d4e083f6517787231e81c7ac1c7bcfd4582
Joel Bartlett <joel@murbar.com>
2019-01-28 18:24:23 -0500

    Complete stretch project

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -2,25 +2,44 @@ function DigitalTimer(config) {
     this.config = config;
     this.elapsedMs = 0;
 
-    this.init = function() {
-        this.interval = setInterval(this.main, this.config.interval);
+    this.init = () => {
+        this.interval = setInterval(this.main, this.config.intervalMs);
     };
-    this.parseMs = function(ms) {
+    
+    this.updateDisplay = () => {
+        const timeString = this.elapsedMs.toString().padStart(5, '0');
         
+        this.config.secondTens.textContent = timeString.charAt(0);
+        this.config.secondOnes.textContent = timeString.charAt(1);
+        this.config.msHundreds.textContent = timeString.charAt(2);
+        this.config.msTens.textContent = timeString.charAt(3);
     };
-    this.main = function() {
 
+    this.main = () => {
+        this.elapsedMs += this.config.intervalMs;
+
+        const stopCondition = this.elapsedMs >= this.config.stopMs + 1;
+
+        if (stopCondition) {
+            this.stop();
+        } else {
+            this.updateDisplay(this.elapsedMs);
+        }
     };
-    this.stop = function() {
+    
+    this.stop = () => {
+        this.config.timeEl.style.color = 'red';
         clearInterval(this.interval);
     };
 }
 
 const app = new DigitalTimer({
-    intervalMs: 30,
+    intervalMs: 10,
+    stopMs: 10000,
     timeEl: document.querySelector('body > div.digits'),
     secondTens: document.getElementById('secondTens'), 
     secondOnes: document.getElementById('secondOnes'), 
     msHundreds: document.getElementById('msHundreds'), 
     msTens: document.getElementById('msTens')
 })
+app.init();
