290f60450b59a8eb8954801e291266c80171585d
Joel Bartlett <joel@murbar.com>
2019-01-28 15:26:34 -0500

    Add nav links

--- js/index.js
+++ js/index.js
@@ -40,3 +40,9 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// nav
+const navLinks = document.querySelectorAll('nav a');
+navLinks.forEach((link, i) => {
+  link.textContent = siteContent.nav[`nav-item-${i + 1}`];
+});
